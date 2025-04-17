4f37b5fd8c9f8130584c4ecdb2e3a3896e15c13b
AnhHQ98 <haquanganh12a1@gmail.com>
2025-04-17 16:36:52 +0700

    const isAfterSentenceEnd = ['.', '...', '!', '?'].includes(previousChar);

--- src/layouts/components/FilmInfo/FilmInfo1.js
+++ src/layouts/components/FilmInfo/FilmInfo1.js
@@ -4,7 +4,7 @@ import styles from './FilmInfo.module.scss';
 const cx = classNames.bind(styles);
 
 function FilmInfo1({ film_json, sceneContent, onSceneClick }) {
-    const [wordClass, setWordClass] = useState({});
+    const [wordClass, setWordClass] = useState();
     useEffect(() => {
         fetch('http://localhost:1025/wordclass')
             .then((res) => res.json())
@@ -30,25 +30,47 @@ function FilmInfo1({ film_json, sceneContent, onSceneClick }) {
             </ol>
             {Array.isArray(sceneContent) && sceneContent.length > 0 && (
                 <div className={cx('sceneContent')}>
-                    {sceneContent.map((shot, shotNumber) => (
-                        <div className={cx('shot')} key={shotNumber}>
-                            <div className={cx('character')}>
-                                <span className={cx('characterName')}>{shot.character.name}</span>
-                            </div>
-                            <div className={cx('subtitle')}>
-                                <div className={cx('vietSub')}>
-                                    <label className={cx('vietSubMeaning')}>{shot.subtitle.vietSub}</label>
+                    {sceneContent.map((shot, shotNumber) => {
+                        const engSubWords = shot.subtitle.engSub.match(/[a-zA-Z0-9]+|[.,!?…]+|\s+/g);
+                        return (
+                            <div className={cx('shot')} key={shotNumber}>
+                                <div className={cx('character')}>
+                                    <span className={cx('characterName')}>{shot.character.name}</span>
                                 </div>
-                                <div>
-                                    <input
-                                        className={cx('engSubWord')}
-                                        placeholder={shot.subtitle.engSub}
-                                        size={shot.subtitle.engSub.length}
-                                    />
+                                <div className={cx('subtitle')}>
+                                    <div className={cx('vietSub')}>
+                                        <label className={cx('vietSubWord')}>{shot.subtitle.vietSub}</label>
+                                    </div>
+                                    <div className={cx('engSub')}>
+                                        {engSubWords.map((engSubWord, index) => {
+                                            const previousChar = engSubWords[index - 1] || null;
+                                            const isFirstChar = index === 0;
+                                            const isAlphanumericChar = /^[a-zA-Z0-9]+$/.test(engSubWord);
+                                            const isAfterSentenceEnd = ['.', '...', '!', '?'].includes(previousChar);
+                                            const englishWord =
+                                                isAlphanumericChar && (isFirstChar || isAfterSentenceEnd)
+                                                    ? engSubWord.toLowerCase()
+                                                    : engSubWord;
+                                            if (wordClass?.hasOwnProperty(englishWord)) {
+                                                console.log('✅ Khớp wordClass:', englishWord);
+                                            }
+                                            if (/[.,!?…\s]/.test(engSubWord)) {
+                                                return <span key={index}>{engSubWord}</span>;
+                                            }
+                                            return (
+                                                <input
+                                                    key={index}
+                                                    className={cx('engSubWord')}
+                                                    placeholder={engSubWord}
+                                                    size={engSubWord.length}
+                                                />
+                                            );
+                                        })}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        );
+                    })}
                 </div>
             )}
         </div>
